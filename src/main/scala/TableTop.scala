ecf4b9e28b3010b321b39516afad3d2359c72eac
matthreif <matthias@reif.id.au>
2018-11-25 20:38:19 +1100

    Implement method to check if given position is on table top. Pass existing tests.

--- src/main/scala/TableTop.scala
+++ src/main/scala/TableTop.scala
@@ -1,3 +1,5 @@
  case class TableTop(width: Int, depth: Int) {
-   def isOn(pos: Position): Boolean = ???
+   def isOn(pos: Position): Boolean = {
+      pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < depth
+   }
 }
